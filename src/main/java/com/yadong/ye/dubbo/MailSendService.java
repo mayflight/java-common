7a3d42e4da3478354da277ac69b3de9b9dbd2621
mayflight <18868824465@163.com>
2018-10-10 11:10:22 +0800

    返回值

--- src/main/java/com/yadong/ye/dubbo/MailSendService.java
+++ src/main/java/com/yadong/ye/dubbo/MailSendService.java
@@ -1,11 +1,12 @@
 package com.yadong.ye.dubbo;
 
+import com.yadong.ye.bean.BaseResult;
 import com.yadong.ye.bean.MailDetailData;
 
 /**
  * @author Ton
  */
 public interface MailSendService {
-    public String sendSimpleMail(MailDetailData data);
+    public BaseResult sendSimpleMail(MailDetailData data);
 
 }
